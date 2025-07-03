b171fcb8e63c2fc1757f8d032c9baccc63dc6c1a
zukahm2005 <145833253+zukahm2005@users.noreply.github.com>
2025-07-03 16:28:58 +0700

    .

--- baikiemtra/demo/src/main/java/com/example/demo/repository/PlaceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Place;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceRepository extends JpaRepository<Place, Long> {}
--- baitest/android/app/src/main/kotlin/com/example/baitest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.baitest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
